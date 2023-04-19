e6de5ea099ecd23d82aa9fd215ecffd7c00cc99d
ATUL TINGRE <atultingre.work@gmail.com>
2023-04-19 17:09:16 +0530

    resovle the useEffct twice api req

--- src/components/Following/Following.js
+++ src/components/Following/Following.js
@@ -1,42 +1,50 @@
-import React, { useEffect, useState} from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { FaLink } from "react-icons/fa";
 import { GoSmiley } from "react-icons/go";
 
 const Following = ({ username }) => {
   const [isLoading, setIsLoading] = useState(true);
   const [following, setFollowing] = useState([]);
+  const effectRan = useRef(false);
 
   useEffect(() => {
-    const fetchFollowing = async () => {
-      setIsLoading(true);
-      try {
-        const response = await fetch(
-          `https://api.github.com/users/${username}/following`
-        );
-        if (response.ok) {
-          const data = await response.json();
-          setFollowing(data);
+    if (effectRan.current === false) {
+      const fetchFollowing = async () => {
+        setIsLoading(true);
+        try {
+          const response = await fetch(
+            `https://api.github.com/users/${username}/following`
+          );
+          if (response.ok) {
+            const data = await response.json();
+            // console.log(data)
+            setFollowing(data);
+            setIsLoading(false);
+          } else {
+            setIsLoading(false);
+            console.error(
+              "Error fetching following list:",
+              response.statusText
+            );
+          }
+        } catch (error) {
           setIsLoading(false);
-        } else {
-          setIsLoading(false);
-          console.error("Error fetching following list:", response.statusText);
+          console.error("Error fetching following list:", error);
         }
-      } catch (error) {
-        setIsLoading(false);
-        console.error("Error fetching following list:", error);
-      }
-    };
-    setTimeout(()=>{
-      fetchFollowing();
-    },1000)
-  }, [username,setIsLoading]);
+      };
+      setTimeout(() => {
+        fetchFollowing();
+      }, 1000);
 
-  
-
+      return () => {
+        effectRan.current = true;
+      };
+    }
+  }, [username, setIsLoading]);
 
   if (isLoading) {
     const skeletons = Array.from({ length: 6 }).map((_, index) => (
-        <div className="card follower-skeleton" key={index}>
+      <div className="card follower-skeleton" key={index}>
         <div className="skeleton avatar-skeleton"></div>
         <div className="skeleton title-skeleton"></div>
       </div>
@@ -62,8 +70,8 @@ const Following = ({ username }) => {
         {isLoading ? (
           <div className="card follower-skeleton">
             <div className="card-body">
-                <div className="skeleton avatar-skeleton"></div>
-                <div className="skeleton title-skeleton"></div>
+              <div className="skeleton avatar-skeleton"></div>
+              <div className="skeleton title-skeleton"></div>
             </div>
           </div>
         ) : following.length > 0 ? (
@@ -71,11 +79,22 @@ const Following = ({ username }) => {
             <div className=" card" key={user.login}>
               <article className="follower-card">
                 <figure className="avatar-circle img-holder">
-                  <img className="img-cover"  width="56px"  height="56px" loading="lazy" src={user.avatar_url} alt={user.login} />
+                  <img
+                    className="img-cover"
+                    width="56px"
+                    height="56px"
+                    loading="lazy"
+                    src={user.avatar_url}
+                    alt={user.login}
+                  />
                 </figure>
                 <h3 className="card-title">{user.login}</h3>
                 <button className="icon-btn">
-                  <a href={user.html_url} target="_blank" rel="noreferrer" className="material-symbols-rounded">
+                  <a
+                    href={user.html_url}
+                    target="_blank"
+                    rel="noreferrer"
+                    className="material-symbols-rounded">
                     <FaLink />
                   </a>
                 </button>
--- src/components/Repository/Repository.js
+++ src/components/Repository/Repository.js
@@ -1,32 +1,38 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { BiCodeBlock, BiGitRepoForked } from "react-icons/bi";
 import { BsFillStarFill } from "react-icons/bs";
 import { GoSmiley } from "react-icons/go";
-import {numberToKilo} from "../Functions.js"
-
-
+import { numberToKilo } from "../Functions.js";
 
 const Repository = ({ username }) => {
-
   const [repositories, setRepositories] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const effectRan = useRef(false);
 
   useEffect(() => {
-    const fetchRepositories = async () => {
-      try {
-        const response = await fetch(`https://api.github.com/users/${username}/repos`);
-        const data = await (response.json());
-        setRepositories(data);
-        setIsLoading(false);
-        // console.log(data)
-      } catch (error) {
-        console.error("Error fetching repositories: ", error);
-        setIsLoading(false);
-      }
-    };
-    setTimeout(()=>{
-      fetchRepositories()
-    },1000)
+    if (effectRan.current === false) {
+      const fetchRepositories = async () => {
+        try {
+          const response = await fetch(
+            `https://api.github.com/users/${username}/repos`
+          );
+          const data = await response.json();
+          setRepositories(data);
+          setIsLoading(false);
+          // console.log(data)
+        } catch (error) {
+          console.error("Error fetching repositories: ", error);
+          setIsLoading(false);
+        }
+      };
+      setTimeout(() => {
+        fetchRepositories();
+      }, 1000);
+
+      return () => {
+        effectRan.current = true;
+      };
+    }
   }, [username]);
 
   if (isLoading) {
@@ -66,48 +72,56 @@ const Repository = ({ username }) => {
             </div>
           </div>
         ) : repositories.length > 0 ? (
-        repositories.map((repo) => (
-          <article className="card repo-card" key={repo.id}>
-            <div className="card-body">
-              <a
-                href={repo.html_url}
-                target="_blank"
-                rel="noreferrer"
-                className="card-title">
-                <h3 className="title-3">{repo.name}</h3>
-              </a>
-              <p className="card-text">{repo.description ? repo.description : "" }</p>
-              <span className="badge">{repo.isPrivate ? "Private" : "Public"}</span>
-            </div>
-            <div className="card-footer">
-              <div className="meta-item">
-                <span className="material-symbols-rounded">
-                  <BiCodeBlock />
+          repositories.map((repo) => (
+            <article className="card repo-card" key={repo.id}>
+              <div className="card-body">
+                <a
+                  href={repo.html_url}
+                  target="_blank"
+                  rel="noreferrer"
+                  className="card-title">
+                  <h3 className="title-3">{repo.name}</h3>
+                </a>
+                <p className="card-text">
+                  {repo.description ? repo.description : ""}
+                </p>
+                <span className="badge">
+                  {repo.isPrivate ? "Private" : "Public"}
                 </span>
-                <span className="span">{repo.language ? repo.language : ""}</span>
               </div>
-              <div className="meta-item">
-                <span className="material-symbols-rounded">
-                  <BsFillStarFill />
-                </span>
-                <span className="span">{numberToKilo(repo.stargazers_count)}</span>
+              <div className="card-footer">
+                <div className="meta-item">
+                  <span className="material-symbols-rounded">
+                    <BiCodeBlock />
+                  </span>
+                  <span className="span">
+                    {repo.language ? repo.language : ""}
+                  </span>
+                </div>
+                <div className="meta-item">
+                  <span className="material-symbols-rounded">
+                    <BsFillStarFill />
+                  </span>
+                  <span className="span">
+                    {numberToKilo(repo.stargazers_count)}
+                  </span>
+                </div>
+                <div className="meta-item">
+                  <span className="material-symbols-rounded">
+                    <BiGitRepoForked />
+                  </span>
+                  <span className="span">{numberToKilo(repo.forks)}</span>
+                </div>
               </div>
-              <div className="meta-item">
-                <span className="material-symbols-rounded">
-                  <BiGitRepoForked />
-                </span>
-                <span className="span">{numberToKilo(repo.forks)}</span>
-              </div>
-            </div>
-          </article>
-        ))
+            </article>
+          ))
         ) : (
           <div className="tab-panel">
             <div className="error-content">
               <p className="title-1">
-              Oops!
-              <GoSmiley style={{ display: "inline" }} />
-            </p>
+                Oops!
+                <GoSmiley style={{ display: "inline" }} />
+              </p>
               <p className="text">Doesn't have any public repositories yet.</p>
             </div>
           </div>
@@ -120,16 +134,16 @@ const Repository = ({ username }) => {
 export default Repository;
 
 // isLoading ? (
-  // <div className="card repo-skeleton">
-  //   <div className="card-body">
-  //     <div className="skeleton title-skeleton"></div>
-  //     <div className="skeleton text-skeleton text-1"></div>
-  //     <div className="skeleton text-skeleton text-2"></div>
-  //   </div>
-  // </div>
+// <div className="card repo-skeleton">
+//   <div className="card-body">
+//     <div className="skeleton title-skeleton"></div>
+//     <div className="skeleton text-skeleton text-1"></div>
+//     <div className="skeleton text-skeleton text-2"></div>
+//   </div>
+// </div>
 // ) : (
 
-  // <div className="error-content">
-  //   <p className="title-1">Oops! :(</p>
-  //   <p className="text">Doesn't have any public repositories yet.</p>
-  //  </div>
+// <div className="error-content">
+//   <p className="title-1">Oops! :(</p>
+//   <p className="text">Doesn't have any public repositories yet.</p>
+//  </div>
